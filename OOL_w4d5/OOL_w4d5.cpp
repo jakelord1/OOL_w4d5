1dc9af53e111fc264ec23adf9b4eece78dee8ca5
jakelord1 <jakelord1.vo@gmail.com>
2023-09-29 14:30:10 +0300

    Добавьте файлы проекта.

---
+++ OOL_w4d5/OOL_w4d5.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream f;
+    string path;
+    cin >> path;
+
+    int errors = 0;
+    bool clbracks = true;
+    f.open(path, ios::in);
+    if (f) {
+        do {
+            char ch = '\0';
+            f.get(ch);
+            if (ch == '<' && clbracks == true) {
+                clbracks = false;
+            }
+            else if (ch == '>' && clbracks == false) {
+                clbracks = true;
+            }
+            else if (ch == '<' && clbracks == false || ch == '>' && clbracks == true) {
+                errors++;
+            }
+            
+
+        } while (f);
+        cout << errors;
+        f.close();
+    }
+}
